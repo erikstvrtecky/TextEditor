9aac1d2626073b5ace860577e1d14de882e54f4f
Erik <stvrtecky.erik@gmail.com>
2020-10-21 08:55:41 +0200

    Initial commit

---
+++ src/TextEditorController.java
@@ -0,0 +1,16 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+import java.awt.event.ActionEvent;
+
+public class TextEditorController {
+
+    @FXML
+    TextArea textFile;
+
+
+    @FXML
+    public void save(ActionEvent actionEvent) {
+
+    }
+}
